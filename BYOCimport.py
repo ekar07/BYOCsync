b45bd08dd7c9801f4b835849af9d77c8de25e0f9
ekar07 <macadmin@Eds-Mac-Pro.local>
2014-02-24 11:26:31 -0600

    generate log, incomplete telnet integration

--- BYOCimport.py
+++ BYOCimport.py
@@ -1,5 +1,9 @@
-import array, csv, os
+import array, csv, os, telnetlib, getpass, time, datetime
 
+#init variables
+byoc=[]
+
+#init class of BYOC device
 class device(object):
     def __init__(self, date, lastname, firstname, gradyear, device, mac, comments, permission, dateadded):
         self.date = date
@@ -12,29 +16,59 @@ class device(object):
         self.permission=permission
         self.dateadded=dateadded
 
-byoc=[]
+#initialize log with current timestamp
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+log = open('/Users/macadmin/BYOCsync/log_byoc_sync.txt', 'a+')
+log.write('---------------- ')
+log.write(st)
+log.write(' ----------------\n')
 
+#start telnet session
+user = raw_input("User: ")
+password = getpass.getpass()
 
-with open('/Users/macadmin/Downloads/TECH DEPT BYOC  - Sheet1.csv', 'rU') as f:
+#read byoc export from google
+
+with open('/Volumes/DATA/downloads/TECH DEPT BYOC  - Sheet1.csv', 'rU') as f:
     reader = csv.reader(f)
     for row in reader:
         this_instance=device(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
         byoc.append(this_instance)
 
-f = open('/Users/macadmin/desktop/python projects/BYOC IMPORT/byocimport.txt', 'w+')
-for x in range(1,len(byoc)):
-	if byoc[x].dateadded == '':
-         f.write('config macfilter add ')
-         f.write(byoc[x].mac)
-         f.write(' 8 byoc-phs \"')
-         f.write(byoc[x].gradyear)
-         f.write(' ')
-         f.write(byoc[x].firstname)
-         f.write(' ')
-         f.write(byoc[x].lastname)
-         f.write(' ')
-         f.write(byoc[x].device)
-         f.write('\"')
-         f.write('\n')
-f.close()
-os.remove("/Users/macadmin/Downloads/TECH DEPT BYOC  - Sheet1.csv")
+q = open('/Users/macadmin/BYOCsync/byocimport.txt', 'w+')
+
+for x in range(0,len(byoc)):
+    '''
+    tn = telnetlib.Telnet('10.10.3.16')
+    tn.read_until("User: ")
+    tn.write(user + "\n")
+    if password:
+        tn.read_until("Password:")
+        tn.write(password + "\n")
+        '''
+    if byoc[x].dateadded == '' and byoc[x].permission == "Yes" :
+        s = 'config macfilter add '
+        s += byoc[x].mac
+        s += ' 8 byoc-phs \"' 
+        s += byoc[x].gradyear 
+        s += ' ' 
+        s += byoc[x].firstname 
+        s += ' ' 
+        s += byoc[x].lastname 
+        s += ' ' 
+        s += byoc[x].device 
+        s += '\"' 
+        s += '\n' 
+        q.write(s + '\n')
+        log.write(s + '\n')
+        '''tn.write(s + '\n')
+tn.write("save config \n")
+tn.write("y")
+tn.write("logout \n")'''
+q.close()
+log.close()
+os.remove("/Volumes/DATA/downloads/TECH DEPT BYOC  - Sheet1.csv")
+
+
+
